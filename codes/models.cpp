7b0deca3eedb693ca9d48441f387bc65d4ef0a42
Capdrake <bastienleuwers2@gmail.com>
2022-07-20 20:07:26 +0200

    Update models.cpp

--- codes/models.cpp
+++ codes/models.cpp
@@ -6,7 +6,7 @@ LinearRegression::LinearRegression(bool is_classification) {
 }
 
 DLLEXPORT void LinearRegression::fit(float *all_samples_inputs,int32_t num_sample, int32_t num_features) {
-
+	
 }
 
 DLLEXPORT float *LinearRegression::predict(float *sample_inputs, int32_t num_features, bool is_classification) {
